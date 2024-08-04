cc4644b0f2512f4b2fb53efb1f23deedc5421752
vasiliyvn <mishadasha1114@gmail.com>
2024-08-04 22:36:36 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def get_multiplied_digits(number):
+    str_number = str(number)
+    if len(str_number)>1:
+        first = int(str_number[0])
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return int(str_number)
+
+
+result = get_multiplied_digits(40203)
+print(result)
